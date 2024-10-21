6bdddf410e1b678191c958679430522236ee6623
Amelia Rae Orodio <ameliarae.orodio@stonybrook.edu>
2024-10-21 16:26:57 -0400

    making the description smaller based on screen size

--- src/work screens/works.js
+++ src/work screens/works.js
@@ -11,12 +11,29 @@ import Typewriter from 'typewriter-effect';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {Tooltip, tooltipClasses} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+import {useMediaQuery} from '@mui/material';
 function WorksScreen(){
     const [hoveredStar, setHoveredStar] = useState(null)
     const navigate = useNavigate();
     const handleBackClick = () => {
         navigate('/');
     };
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    const isMediumScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+    const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
+    
+    const getDescription = (descriptions) => {
+        if (isSmallScreen) {
+          return descriptions.small;
+        } else if (isMediumScreen) {
+          return descriptions.medium;
+        } else if (isLargeScreen) {
+          return descriptions.large;
+        }
+      };
+
     const PinkTooltip = styled(({ className, ...props }) => (
         <Tooltip {...props} classes={{ popper: className }} />
       ))(({ theme }) => ({
@@ -32,7 +49,13 @@ function WorksScreen(){
             height: 150,
             img: "",
             title:"Current Portfolio",
-            description:"This portfolio is a reflection of my journey as a creative technologist, where I explore the exciting intersection of art and technology. Each project within is a testament to pushing the boundaries of both fields, blending code with aesthetic expression. Using tools beyond the conventional—like interactive components, animations, and generative design—this collection represents my evolution from technical development to crafting experiences that feel both personal and immersive. Here, I challenge the norm and experiment with web interfaces to create digital spaces that feel alive, whimsical, and deeply connected to my creative vision.",
+            descriptions: {
+                small: 'A portfolio exploring tech and creativity.',
+                medium:
+                  'This portfolio reflects my journey as a creative technologist, blending art and technology.',
+                large:
+                  'This portfolio is a reflection of my journey as a creative technologist, where I explore the exciting intersection of art and technology. Each project within is a testament to pushing the boundaries of both fields, blending code with aesthetic expression. Using tools beyond the conventional—like interactive components, animations, and generative design—this collection represents my evolution from technical development to crafting experiences that feel both personal and immersive. Here, I challenge the norm and experiment with web interfaces to create digital spaces that feel alive, whimsical, and deeply connected to my creative vision.',
+              },
             year:"2024",
             technologies:"Javascript, CSS, HTML, ReactJS, NodeJS",
             link: "https://github.com/ameliaraeorodio/insidemymind"
@@ -44,7 +67,11 @@ function WorksScreen(){
             height: 150,
             img: "",
             title:"Playlister",
-            description:"Playlister is a dynamic web app designed to help users curate and share personalized playlists using YouTube videos. With an intuitive interface, users can easily compile and organize their favorite content into themed playlists. Once created, these playlists can be published to a vibrant online forum, allowing community interaction, feedback, and discovery of new content. Playlister blends multimedia with social engagement, empowering users to express their tastes while connecting with others through shared experiences in music, education, entertainment, and more.",
+            descriptions:{
+                small:" Create and share playlists using YouTube videos",
+                medium: "Playlister is an app for curating and sharing YouTube video playlists with an online community.",
+                large:"Playlister is a dynamic web app designed to help users curate and share personalized playlists using YouTube videos. With an intuitive interface, users can easily compile and organize their favorite content into themed playlists. Once created, these playlists can be published to a vibrant online forum, allowing community interaction, feedback, and discovery of new content. Playlister blends multimedia with social engagement, empowering users to express their tastes while connecting with others through shared experiences in music, education, entertainment, and more.",
+            },
             year:"2022",
             technologies:"Javascript, ReactJS, MongoDB, NodeJS, Express, HTML, CSS",
             link: "https://github.com/ameliaraeorodio/Playlister"
@@ -56,6 +83,11 @@ function WorksScreen(){
             height: 150,
             img: "",
             title:"ExploreX",
+            descriptions:{
+                small:" A map app for visualizing user-generated data.",
+                medium: "Explore X is an app that allows users to upload maps, visualize data, and interact with custom visualizations.",
+                large:"Explore X is a dynamic map rendering app designed for users to creatively visualize geographic data. Users can upload map data (specifically SHP/DBF, GeoJSON, and KML files) and transform it into compelling visuals, such as heat or spike maps, that highlight key information. The app offers a range of customization options, enabling users to fine-tune the appearance of their maps. Beyond personal use, Explore X fosters a community where users can explore, interact with, and export each other's maps, creating a collaborative environment for geographic data exploration and creativity. This project was created by me alongside a team of three talented individuals Juan Ospina, Jacob Niyazov, and Jesse Wang. ",
+            },
             description:"Explore X is a dynamic map rendering app designed for users to creatively visualize geographic data. Users can upload map data (specifically SHP/DBF, GeoJSON, and KML files) and transform it into compelling visuals, such as heat or spike maps, that highlight key information. The app offers a range of customization options, enabling users to fine-tune the appearance of their maps. Beyond personal use, Explore X fosters a community where users can explore, interact with, and export each other's maps, creating a collaborative environment for geographic data exploration and creativity. This project was created by me alongside a team of three talented individuals Juan Ospina, Jacob Niyazov, and Jesse Wang. ",
             year:"2023",
             technologies:"Javascript, ReactJS, MongoDB, NodeJS, Express, HTML, CSS",
@@ -65,7 +97,11 @@ function WorksScreen(){
             height: 150,
             img: "",
             title:"Old Portfolio",
-            description:"My old portfolio was my first step into web development, crafted before I delved deep into building web applications. Built using Bootstrap, vanilla JavaScript, HTML, and CSS, this portfolio showcased my early projects and reflected my initial understanding of design and coding. It served as a minimalist, functional platform to present my skills and creativity, laying the foundation for the more dynamic and complex web applications I would later go on to create.",
+            descriptions:{
+                small:" My first portfolio was a minimalist site showcasing my early projects and setting the stage for more complex work to come.",
+                medium: "My old portfolio marked my early venture into web development. It was a minimalist site created with Bootstrap, vanilla JavaScript, HTML, and CSS, showcasing my initial projects. This platform reflected my budding skills in design and coding, laying the groundwork for the more advanced web applications I would build later. It represents my transition from simple layouts to more complex, interactive projects.",
+                large:"My old portfolio was my first step into web development, crafted before I delved deep into building web applications. Built using Bootstrap, vanilla JavaScript, HTML, and CSS, this portfolio showcased my early projects and reflected my initial understanding of design and coding. It served as a minimalist, functional platform to present my skills and creativity, laying the foundation for the more dynamic and complex web applications I would later go on to create.",
+            },
             year:"2022",
             technologies:"Javascript, HTML, CSS, Bootstrap",
             link: "https://github.com/ameliaraeorodio/ameliaraeorodio.github.io"
@@ -151,7 +187,7 @@ function WorksScreen(){
                                 {item.technologies}
                             </Typography>
                             <Typography sx = {{fontFamily: "Cormorant, serif",padding:"2vh"}}>
-                                {item.description}
+                            {getDescription(item.descriptions)}
                             </Typography>
                         </a>
                     </Box>
